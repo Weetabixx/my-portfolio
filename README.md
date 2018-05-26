134432fab56dd452081ccdb376e393ecbefa4459
Robin Graf <robingraf@hotmail.co.uk>
2018-05-26 13:48:40 +0000

    moved django project to this repo and installed python3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # my-portfolio
+must install:
+
+python3.4 +
+django 2.0.5 +
+sqlite3
+
+
+run with:
+python3 manage.py runserver $IP:$PORT
---
+++ mysite/my_page/views.py
@@ -0,0 +1,39 @@
+#imports
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.templatetags.static import static
+
+
+def index(request):
+    imgurl = static('mypage/images/banner.jpg')
+    context = {
+        'bannerhtml':'<img src="' + imgurl + '" width="100%">'
+    }
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context))
+    
+def fourOfour(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+    
+def about(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+    
+def contact(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+    
+def projects(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+    
+def art(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+    
+def pso(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
