2af1a3f01160e55ac3558fdab56c78ebcc419f64
Robin Graf <robingraf@hotmail.co.uk>
2018-05-28 11:30:22 +0000

    added Post model

---
+++ mysite/my_page/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    post_type = models.CharField(max_length=30)  #for example 'art' or 'project'
+    post_title = models.CharField(max_length=30)
+    post_html_text = models.CharField(max_length=5000)
+    post_description = models.CharField(max_length=30)
+    post_date = models.DateTimeField()
+    
+    def save(self, *args, **kwargs):
+        # On save, update timestamps
+        if not self.id:
+            self.date = timezone.now()
+        return super(Post, self).save(*args, **kwargs)
