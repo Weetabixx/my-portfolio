7bb2a1de52b55858ba7164d626fa82b29f0d8737
Robin Graf <robingraf@hotmail.co.uk>
2018-07-07 00:19:57 +0000

    sorted csrf issue

--- mysite/my_page/views.py
+++ mysite/my_page/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from django.templatetags.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
+from django.template import RequestContext
 
 from .forms import PostForm
 
@@ -48,20 +49,36 @@ def pso(request):
     template = loader.get_template('pso.html')
     return HttpResponse(template.render())
     
+
 @login_required(login_url='/admin/')
 def makePostPage(request):
     if request.method != 'POST':
         form = PostForm()
         context = {'form' : form}
         template = loader.get_template('makePost.html')
-        return HttpResponse(template.render(context))
+        return render(request, 'makePost.html', context)
+        #return HttpResponse(template.render(context))
     # create a form instance and populate it with data from the request:
     form = PostForm(request.POST)
     # check whether it's valid:
     if form.is_valid():
         # process the data in form.cleaned_data as required
-        title = form.cleaned_data['post_title']
-        # ...
+        post_type = form.cleaned_data['post_type']
+        post_title = form.cleaned_data['post_title']
+        post_html_text = form.cleaned_data['post_html_text']
+        post_description = form.cleaned_data['post_description']
+        post_confirm = form.cleaned_data['post_confirm']
+        
+        if not post_confirm:
+            form.post_confirm = True
+            context = {
+                'form' : form,
+                'post_title': post_title,
+                'post_html_text': post_html_text
+            }
+            template = loader.get_template('previewPost.html')
+            return render(request, 'previewPost.html', context)
+            
         # redirect to a new URL:
         return HttpResponseRedirect('/')
 
